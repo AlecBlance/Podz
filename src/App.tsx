4a6ed3f1186c8ecf2f8e69ad0ed7e73606f6bdf9
Alec Blance <17378596+AlecBlance@users.noreply.github.com>
2023-09-05 18:16:55 +0800

    refactor: Bootstrap Home Component

--- src/App.tsx
+++ src/App.tsx
@@ -1,77 +1,10 @@
-import Header from "./components /Header";
+import Home from "./components /Home";
 import MobileNavigation from "./components /MobileNavigation";
-import MusicCard from "./components /MusicCard";
-import Section from "./components /Section";
 
 const App = () => {
   return (
     <div className="h-screen flex flex-col">
-      <div className="p-5 grow overflow-y-auto scrollbar-hide">
-        <Header />
-        <Section title="Jump back in">
-          <MusicCard title="Sample" artist="Artist" />
-          <MusicCard
-            title="Circle Timeaaaaaaaaaaaaaaaaaaaaaaaaaa"
-            artist="Dear Media"
-          />
-          <MusicCard
-            title="Circle Timeaaaaaaaaaaaaaaaaaaaaaaaaaa"
-            artist="Dear Media"
-          />
-          <MusicCard
-            title="Circle Timeaaaaaaaaaaaaaaaaaaaaaaaaaa"
-            artist="Dear Media"
-          />
-        </Section>
-        <Section title="Recommended songs">
-          <MusicCard
-            title="Circle Timeaaaaaaaaaaaaaaaaaaaaaaaaaa"
-            artist="Dear Media"
-          />
-          <MusicCard
-            title="Circle Timeaaaaaaaaaaaaaaaaaaaaaaaaaa"
-            artist="Dear Media"
-          />
-          <MusicCard
-            title="Circle Timeaaaaaaaaaaaaaaaaaaaaaaaaaa"
-            artist="Dear Media"
-          />
-          <MusicCard
-            title="Circle Timeaaaaaaaaaaaaaaaaaaaaaaaaaa"
-            artist="Dear Media"
-          />
-        </Section>
-        <Section title="Jump back in">
-          <MusicCard title="Sample" artist="Artist" />
-          <MusicCard
-            title="Circle Timeaaaaaaaaaaaaaaaaaaaaaaaaaa"
-            artist="Dear Media"
-          />
-          <MusicCard
-            title="Circle Timeaaaaaaaaaaaaaaaaaaaaaaaaaa"
-            artist="Dear Media"
-          />
-          <MusicCard
-            title="Circle Timeaaaaaaaaaaaaaaaaaaaaaaaaaa"
-            artist="Dear Media"
-          />
-        </Section>
-        <Section title="Jump back in">
-          <MusicCard title="Sample" artist="Artist" />
-          <MusicCard
-            title="Circle Timeaaaaaaaaaaaaaaaaaaaaaaaaaa"
-            artist="Dear Media"
-          />
-          <MusicCard
-            title="Circle Timeaaaaaaaaaaaaaaaaaaaaaaaaaa"
-            artist="Dear Media"
-          />
-          <MusicCard
-            title="Circle Timeaaaaaaaaaaaaaaaaaaaaaaaaaa"
-            artist="Dear Media"
-          />
-        </Section>
-      </div>
+      <Home />
       <MobileNavigation />
     </div>
   );
