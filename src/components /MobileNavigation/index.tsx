d04db17111dc92615f8f48b8148a7f47300d9061
Alec Blance <17378596+AlecBlance@users.noreply.github.com>
2023-09-05 19:19:23 +0800

    style: search route icon color change

--- src/components /MobileNavigation/index.tsx
+++ src/components /MobileNavigation/index.tsx
@@ -1,7 +1,10 @@
 import PlayingNow from "./PlayingNow";
-import { Link } from "react-router-dom";
+import { Link, useMatch } from "react-router-dom";
 
 const MobileNavigation = () => {
+  const isHome = useMatch("/");
+  const isSearch = useMatch("/search");
+
   return (
     <div className="bg-[#131313] text-custom-neutrals-offwhite">
       <PlayingNow />
@@ -11,10 +14,28 @@ const MobileNavigation = () => {
             xmlns="http://www.w3.org/2000/svg"
             viewBox="0 0 24 24"
             fill="currentColor"
-            className="w-7 h-7 mx-4 text-custom-vibrant-blue"
+            className={`w-7 h-7 mx-4 text-custom-vibrant-blue ${
+              isHome ? "block" : "hidden"
+            }`}
           >
             <path d="M11.47 3.84a.75.75 0 011.06 0l8.69 8.69a.75.75 0 101.06-1.06l-8.689-8.69a2.25 2.25 0 00-3.182 0l-8.69 8.69a.75.75 0 001.061 1.06l8.69-8.69z" />
             <path d="M12 5.432l8.159 8.159c.03.03.06.058.091.086v6.198c0 1.035-.84 1.875-1.875 1.875H15a.75.75 0 01-.75-.75v-4.5a.75.75 0 00-.75-.75h-3a.75.75 0 00-.75.75V21a.75.75 0 01-.75.75H5.625a1.875 1.875 0 01-1.875-1.875v-6.198a2.29 2.29 0 00.091-.086L12 5.43z" />
+          </svg>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={1.5}
+            stroke="currentColor"
+            className={`w-7 h-7 mx-4 text-custom-card-artist ${
+              isHome ? "hidden" : "block"
+            }`}
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M2.25 12l8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25"
+            />
           </svg>
         </Link>
         <Link to="/search">
@@ -24,7 +45,9 @@ const MobileNavigation = () => {
             viewBox="0 0 24 24"
             strokeWidth={1.5}
             stroke="currentColor"
-            className="w-7 h-7 mx-4 text-custom-card-artist"
+            className={`w-7 h-7 mx-4  ${
+              isSearch ? "text-custom-vibrant-blue" : "text-custom-card-artist"
+            }`}
           >
             <path
               strokeLinecap="round"
@@ -40,7 +63,7 @@ const MobileNavigation = () => {
           viewBox="0 0 24 24"
           strokeWidth={1.5}
           stroke="currentColor"
-          className="w-7 h-7 mx-4 text-custom-card-artist"
+          className={`w-7 h-7 mx-4 text-custom-card-artist`}
         >
           <path
             strokeLinecap="round"
