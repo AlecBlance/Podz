f08ff38d91777fc380661e6ccd946fd3f7554843
Alec Blance <17378596+AlecBlance@users.noreply.github.com>
2023-09-05 19:35:02 +0800

    style: outline instead of solid nav icon

--- src/components /MobileNavigation/index.tsx
+++ src/components /MobileNavigation/index.tsx
@@ -12,23 +12,12 @@ const MobileNavigation = () => {
         <Link to="/">
           <svg
             xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 24 24"
-            fill="currentColor"
-            className={`w-7 h-7 mx-4 text-custom-vibrant-blue ${
-              isHome ? "block" : "hidden"
-            }`}
-          >
-            <path d="M11.47 3.84a.75.75 0 011.06 0l8.69 8.69a.75.75 0 101.06-1.06l-8.689-8.69a2.25 2.25 0 00-3.182 0l-8.69 8.69a.75.75 0 001.061 1.06l8.69-8.69z" />
-            <path d="M12 5.432l8.159 8.159c.03.03.06.058.091.086v6.198c0 1.035-.84 1.875-1.875 1.875H15a.75.75 0 01-.75-.75v-4.5a.75.75 0 00-.75-.75h-3a.75.75 0 00-.75.75V21a.75.75 0 01-.75.75H5.625a1.875 1.875 0 01-1.875-1.875v-6.198a2.29 2.29 0 00.091-.086L12 5.43z" />
-          </svg>
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
             fill="none"
             viewBox="0 0 24 24"
             strokeWidth={1.5}
             stroke="currentColor"
-            className={`w-7 h-7 mx-4 text-custom-card-artist ${
-              isHome ? "hidden" : "block"
+            className={`w-7 h-7 mx-4 ${
+              isHome ? "text-custom-vibrant-blue" : "text-custom-card-artist"
             }`}
           >
             <path
