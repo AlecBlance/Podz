05be414cc6396010c674072b84f6479941055c08
Alec Blance <17378596+AlecBlance@users.noreply.github.com>
2023-09-05 18:32:44 +0800

    feat: working search routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,16 @@
 import Home from "./components /Home";
 import MobileNavigation from "./components /MobileNavigation";
+import { Route, Routes } from "react-router-dom";
+import Search from "./components /Search";
 
 const App = () => {
   return (
     <div className="h-screen flex flex-col">
       <div className="p-5 grow overflow-y-auto scrollbar-hide">
-        <Home />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/search" element={<Search />} />
+        </Routes>
       </div>
       <MobileNavigation />
     </div>
