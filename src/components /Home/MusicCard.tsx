48332e6c7c1036f416d996c011986351e38e05e7
Alec Blance <17378596+AlecBlance@users.noreply.github.com>
2023-09-06 19:00:03 +0800

    style: bigger image sizes

--- src/components /Home/MusicCard.tsx
+++ src/components /Home/MusicCard.tsx
@@ -2,8 +2,8 @@ import { MusicCardProps } from "../../types";
 
 const MusicCard = ({ title, artist, image }: MusicCardProps) => {
   return (
-    <div className="w-28 mr-4">
-      <div className="bg-slate-400 w-28 h-28 rounded-md">{image}</div>
+    <div className="w-32 mr-4">
+      <div className="bg-slate-400 w-32 h-32 rounded-md">{image}</div>
       <div>
         <h1 className="text-custom-neutrals-offwhite text-sm truncate mt-1">
           {title}
