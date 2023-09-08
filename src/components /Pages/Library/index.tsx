df381fc81bf361aef4ee7150109a50a851ae3cae
Alec Blance <17378596+AlecBlance@users.noreply.github.com>
2023-09-08 11:43:03 +0800

    style: adjust header size of library

--- src/components /Pages/Library/index.tsx
+++ src/components /Pages/Library/index.tsx
@@ -3,7 +3,7 @@ import SavedMusic from "../../Music/SavedMusic";
 const Library = () => {
   return (
     <div>
-      <div className="bg-library-header bg-cover h-64 bg-bottom flex items-end sticky top-0">
+      <div className="h-36 flex items-end sticky top-0 background">
         <div className="text-custom-neutrals-offwhite text-4xl font-black m-5  bg-black/10 ">
           <p>MY LIBRARY</p>
         </div>
