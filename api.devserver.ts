415eea96ccd3769f0b282929a5fec1feb191f2c3
Alec Blance <17378596+AlecBlance@users.noreply.github.com>
2023-09-12 16:41:36 +0800

    revert: play youtube audio

--- api.devserver.ts
+++ api.devserver.ts
@@ -1,9 +1,7 @@
 import app from "./api.app.js";
 import searchRouter from "./api/routes/search.js";
-import playRouter from "./api/routes/play.js";
 
 app.use("/api/search", searchRouter);
-app.use("/api/play", playRouter);
 
 app.listen(3001, () => {
   console.log("Server running at http://localhost:3001");
--- api/play.ts
+++
@@ -1,6 +0,0 @@
-import app from "../api.app.js";
-import playRouter from "./routes/play.js";
-
-app.use("/api/play", playRouter);
-
-export default app;
