0bfe7798ba3b10f395bf3078cf6e8cacf9bfef57
Alec Blance <17378596+AlecBlance@users.noreply.github.com>
2023-09-12 13:29:33 +0800

    fix: mislooked removal of type

--- src/types.ts
+++ src/types.ts
@@ -1,5 +1,11 @@
 import React from "react";
 
+export interface MusicCardProps {
+  title: string;
+  artist: string;
+  image?: string;
+}
+
 export interface SectionProps {
   children: React.ReactNode;
   title: string;
