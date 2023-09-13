915947bfd10d10c65efc0b8a6aef9393c9b4d2c8
Alec Blance <17378596+AlecBlance@users.noreply.github.com>
2023-09-13 20:12:12 +0800

    feat: play/pause desktop

--- src/components /NowPlaying/Desktop/Footer.tsx
+++ src/components /NowPlaying/Desktop/Footer.tsx
@@ -7,8 +7,9 @@ const Footer = () => {
   const [isPageVisible, setIsPageVisible] = useState<boolean>(false);
   const playing = useAppSelector((state) => state.playing);
   const inputRef = useRef<HTMLInputElement>(null);
-  const { audioRef, updateAudioTime } = useAudioContext();
+  const { audioRef, updateAudioTime, play, pause } = useAudioContext();
   const pageRef = useRef<HTMLInputElement>(null);
+  const isPlaying = useAppSelector((state) => state.audio.isPlaying);
 
   onpopstate = () => {
     if (!isPageVisible) return;
@@ -84,25 +85,49 @@ const Footer = () => {
           )}
         </div>
         <div className="w-1/3 flex justify-center items-center grow">
-          <svg
-            className="w-12 h-12 text-custom-vibrant-blue"
-            viewBox="0 0 24 24"
-            fill="currentcolor"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <rect
-              x="8"
-              y="7"
-              width="9"
-              height="10"
-              className="text-custom-neutrals-offwhite"
-            />
-            <path
-              fillRule="evenodd"
-              clipRule="evenodd"
-              d="M2.25 12C2.25 6.615 6.615 2.25 12 2.25C17.385 2.25 21.75 6.615 21.75 12C21.75 17.385 17.385 21.75 12 21.75C6.615 21.75 2.25 17.385 2.25 12ZM16.274 11.017C16.4492 11.1145 16.5951 11.257 16.6967 11.4299C16.7983 11.6027 16.8519 11.7995 16.8519 12C16.8519 12.2005 16.7983 12.3973 16.6967 12.5701C16.5951 12.743 16.4492 12.8855 16.274 12.983L10.671 16.096C10.4998 16.191 10.3067 16.2397 10.1109 16.2373C9.91513 16.2348 9.72336 16.1813 9.55458 16.082C9.3858 15.9828 9.24586 15.8411 9.14859 15.6712C9.05133 15.5012 9.00011 15.3088 9 15.113V8.887C9 8.03 9.921 7.487 10.671 7.904L16.274 11.017Z"
-            />
-          </svg>
+          {isPlaying && playing.id ? (
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 24 24"
+              fill="currentColor"
+              className="w-12 h-12   text-custom-vibrant-blue"
+              onClick={pause}
+            >
+              <rect
+                x="8"
+                y="7"
+                width="9"
+                height="10"
+                className="text-custom-neutrals-offwhite"
+              />
+              <path
+                fillRule="evenodd"
+                d="M2.25 12c0-5.385 4.365-9.75 9.75-9.75s9.75 4.365 9.75 9.75-4.365 9.75-9.75 9.75S2.25 17.385 2.25 12zM9 8.25a.75.75 0 00-.75.75v6c0 .414.336.75.75.75h.75a.75.75 0 00.75-.75V9a.75.75 0 00-.75-.75H9zm5.25 0a.75.75 0 00-.75.75v6c0 .414.336.75.75.75H15a.75.75 0 00.75-.75V9a.75.75 0 00-.75-.75h-.75z"
+                clipRule="evenodd"
+              />
+            </svg>
+          ) : (
+            <svg
+              className="w-12 h-12 text-custom-vibrant-blue"
+              viewBox="0 0 24 24"
+              fill="currentcolor"
+              xmlns="http://www.w3.org/2000/svg"
+              onClick={play}
+            >
+              <rect
+                x="8"
+                y="7"
+                width="9"
+                height="10"
+                className="text-custom-neutrals-offwhite"
+              />
+              <path
+                fillRule="evenodd"
+                clipRule="evenodd"
+                d="M2.25 12C2.25 6.615 6.615 2.25 12 2.25C17.385 2.25 21.75 6.615 21.75 12C21.75 17.385 17.385 21.75 12 21.75C6.615 21.75 2.25 17.385 2.25 12ZM16.274 11.017C16.4492 11.1145 16.5951 11.257 16.6967 11.4299C16.7983 11.6027 16.8519 11.7995 16.8519 12C16.8519 12.2005 16.7983 12.3973 16.6967 12.5701C16.5951 12.743 16.4492 12.8855 16.274 12.983L10.671 16.096C10.4998 16.191 10.3067 16.2397 10.1109 16.2373C9.91513 16.2348 9.72336 16.1813 9.55458 16.082C9.3858 15.9828 9.24586 15.8411 9.14859 15.6712C9.05133 15.5012 9.00011 15.3088 9 15.113V8.887C9 8.03 9.921 7.487 10.671 7.904L16.274 11.017Z"
+              />
+            </svg>
+          )}
         </div>
         <div className="flex items-center pr-5">
           <svg
@@ -126,6 +151,7 @@ const Footer = () => {
         isPageVisible={isPageVisible}
         setIsPageVisible={setIsPageVisible}
         playing={playing}
+        isPlaying={isPlaying}
         ref={pageRef}
       />
     </div>
--- src/components /NowPlaying/Desktop/Page.tsx
+++ src/components /NowPlaying/Desktop/Page.tsx
@@ -9,9 +9,10 @@ const Page = forwardRef<
     isPageVisible: boolean;
     setIsPageVisible: React.Dispatch<React.SetStateAction<boolean>>;
     playing: SearchResult;
+    isPlaying: boolean;
   }
->(({ isPageVisible, setIsPageVisible, playing }, ref) => {
-  const { updateAudioTime } = useAudioContext();
+>(({ isPageVisible, setIsPageVisible, playing, isPlaying }, ref) => {
+  const { updateAudioTime, play, pause } = useAudioContext();
 
   const hide = {
     top: "100dvh",
@@ -86,25 +87,49 @@ const Page = forwardRef<
             </svg>
           </div>
           <div className="flex justify-center w-1/3">
-            <svg
-              className="w-14 h-14 text-custom-vibrant-blue"
-              viewBox="0 0 24 24"
-              fill="currentcolor"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <rect
-                x="8"
-                y="7"
-                width="9"
-                height="10"
-                className="text-custom-neutrals-offwhite"
-              />
-              <path
-                fillRule="evenodd"
-                clipRule="evenodd"
-                d="M2.25 12C2.25 6.615 6.615 2.25 12 2.25C17.385 2.25 21.75 6.615 21.75 12C21.75 17.385 17.385 21.75 12 21.75C6.615 21.75 2.25 17.385 2.25 12ZM16.274 11.017C16.4492 11.1145 16.5951 11.257 16.6967 11.4299C16.7983 11.6027 16.8519 11.7995 16.8519 12C16.8519 12.2005 16.7983 12.3973 16.6967 12.5701C16.5951 12.743 16.4492 12.8855 16.274 12.983L10.671 16.096C10.4998 16.191 10.3067 16.2397 10.1109 16.2373C9.91513 16.2348 9.72336 16.1813 9.55458 16.082C9.3858 15.9828 9.24586 15.8411 9.14859 15.6712C9.05133 15.5012 9.00011 15.3088 9 15.113V8.887C9 8.03 9.921 7.487 10.671 7.904L16.274 11.017Z"
-              />
-            </svg>
+            {isPlaying && playing.id ? (
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 24 24"
+                fill="currentColor"
+                className="w-14 h-14   text-custom-vibrant-blue"
+                onClick={pause}
+              >
+                <rect
+                  x="8"
+                  y="7"
+                  width="9"
+                  height="10"
+                  className="text-custom-neutrals-offwhite"
+                />
+                <path
+                  fillRule="evenodd"
+                  d="M2.25 12c0-5.385 4.365-9.75 9.75-9.75s9.75 4.365 9.75 9.75-4.365 9.75-9.75 9.75S2.25 17.385 2.25 12zM9 8.25a.75.75 0 00-.75.75v6c0 .414.336.75.75.75h.75a.75.75 0 00.75-.75V9a.75.75 0 00-.75-.75H9zm5.25 0a.75.75 0 00-.75.75v6c0 .414.336.75.75.75H15a.75.75 0 00.75-.75V9a.75.75 0 00-.75-.75h-.75z"
+                  clipRule="evenodd"
+                />
+              </svg>
+            ) : (
+              <svg
+                className="w-14 h-14 text-custom-vibrant-blue"
+                viewBox="0 0 24 24"
+                fill="currentcolor"
+                xmlns="http://www.w3.org/2000/svg"
+                onClick={play}
+              >
+                <rect
+                  x="8"
+                  y="7"
+                  width="9"
+                  height="10"
+                  className="text-custom-neutrals-offwhite"
+                />
+                <path
+                  fillRule="evenodd"
+                  clipRule="evenodd"
+                  d="M2.25 12C2.25 6.615 6.615 2.25 12 2.25C17.385 2.25 21.75 6.615 21.75 12C21.75 17.385 17.385 21.75 12 21.75C6.615 21.75 2.25 17.385 2.25 12ZM16.274 11.017C16.4492 11.1145 16.5951 11.257 16.6967 11.4299C16.7983 11.6027 16.8519 11.7995 16.8519 12C16.8519 12.2005 16.7983 12.3973 16.6967 12.5701C16.5951 12.743 16.4492 12.8855 16.274 12.983L10.671 16.096C10.4998 16.191 10.3067 16.2397 10.1109 16.2373C9.91513 16.2348 9.72336 16.1813 9.55458 16.082C9.3858 15.9828 9.24586 15.8411 9.14859 15.6712C9.05133 15.5012 9.00011 15.3088 9 15.113V8.887C9 8.03 9.921 7.487 10.671 7.904L16.274 11.017Z"
+                />
+              </svg>
+            )}
           </div>
           <div className="flex items-center pr-5 w-1/3 justify-end">
             <svg
