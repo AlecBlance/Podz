d9721102d40d1a94117a97865fa54c9a67edb2b3
Alec Blance <17378596+AlecBlance@users.noreply.github.com>
2023-09-15 10:27:06 +0800

    refactor: possible overflow problem

--- src/components /Navigation/RecentMusicList.tsx
+++ src/components /Navigation/RecentMusicList.tsx
@@ -5,22 +5,21 @@ import RecentMusic from "../Music/RecentMusic";
 const RecentMusicList = () => {
   const recent = useAppSelector((state) => state.recent);
   const sectionRef = useRef<HTMLDivElement>(null);
-
-  const section = sectionRef.current;
   const [limit, setLimit] = useState<number>(20);
-
-  useEffect(() => {
-    if (!section) return;
-    setLimit(getLimit(section.offsetHeight));
-    window.addEventListener("resize", () => {
-      setLimit(getLimit(section.offsetHeight));
-    });
-  }, [section]);
 
   const getLimit = (height: number) => {
     const shouldBeLimit = Math.floor(height / 60);
     return height % 60 ? shouldBeLimit - 1 : shouldBeLimit;
   };
+
+  useEffect(() => {
+    const section = sectionRef.current;
+    if (!section) return;
+    setLimit(getLimit(section.offsetHeight));
+    window.addEventListener("resize", () => {
+      setLimit(getLimit(section.offsetHeight));
+    });
+  }, [sectionRef]);
 
   return (
     <div className="h-full overflow-hidden" ref={sectionRef}>
