ab903af499d922af3a47e902fa113e7fd719f213
Alec Blance <17378596+AlecBlance@users.noreply.github.com>
2023-09-22 11:17:07 +0800

    fix: inconsistent music card

--- src/components /Pages/Home/Header.tsx
+++ src/components /Pages/Home/Header.tsx
@@ -1,6 +1,8 @@
-const Header = () => {
+import { RefObject } from "react";
+
+const Header = ({ innerRef }: { innerRef: RefObject<HTMLDivElement> }) => {
   return (
-    <div className="text-custom-neutrals-offwhite">
+    <div className="text-custom-neutrals-offwhite" ref={innerRef}>
       <h1 className="text-2xl  font-semibold ">Hey, User!</h1>
       <p className="text-xs  font-light mt-2 ">
         Check out this week's fresh releases.
