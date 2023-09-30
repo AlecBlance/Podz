2d52367e978389d9a47eab48a6533d0dc54fb0b1
Alec Blance <17378596+AlecBlance@users.noreply.github.com>
2023-09-30 19:25:39 +0800

    feat: optimized search Closes #31

--- src/services/search.ts
+++ src/services/search.ts
@@ -1,13 +1,23 @@
-import axios from "axios";
+import axios, { CancelTokenSource } from "axios";
 import { SearchResult } from "../types";
 
+let cancelToken: CancelTokenSource;
+
 const search = async (query: string, filter?: { single: boolean }) => {
+
+  if (typeof cancelToken != typeof undefined) {
+    cancelToken.cancel("Operation canceled due to new request.");
+  }
+
+  cancelToken = axios.CancelToken.source();
+
+
   let url =
     import.meta.env.MODE === "development"
       ? `http://localhost:3001/api/search/?query=${query}`
       : `/api/search/?query=${query}`;
   if (filter?.single) url = `${url}&single`;
-  const result = await axios.get<SearchResult[] | SearchResult>(url);
+  const result = await axios.get<SearchResult[] | SearchResult>(url, { cancelToken: cancelToken.token });
   return result.data;
 };
 
